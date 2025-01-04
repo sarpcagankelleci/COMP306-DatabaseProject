bdfa8b0697f946ac13c9ce34c218795207ed1f53
Sarp Çağan Kelleci <126450395+sarpcagankelleci@users.noreply.github.com>
2025-01-04 19:55:19 +0300

    Fix

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import csv
 db_connection = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="",
+    passwd="Sultan9988",
     auth_plugin='mysql_native_password'
 )
 db_cursor = db_connection.cursor(buffered=True)
