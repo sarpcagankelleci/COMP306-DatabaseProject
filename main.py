ea2c648f9783d236ccd9c2e9f0caf2c7fa39e849
kugur21 <kugur21@ku.edu.tr>
2025-01-17 17:12:36 +0300

    -

--- main.py
+++ main.py
@@ -19,11 +19,13 @@ import csv
 db_connection = mysql.connector.connect(
   host="localhost",
   user="root",
-  passwd="Fatura10*",
+  passwd="ksu12345",
   auth_plugin='mysql_native_password'
 )
 print(db_connection)
 db_cursor = db_connection.cursor(buffered=True)
+
+#deneme
 
 # Helper Function: Populate Table from CSV
 def populate_table(file_path, insert_query):
